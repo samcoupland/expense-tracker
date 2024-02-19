a0c792f28c0cd8f11f8906d52cf2b85bf9417f91
Sam Coupland <samcoupland@gmail.com>
2024-02-19 14:22:54 +0000

    feat: intial

---
+++ src/components/Balance.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>Your Balance</h4>
+  <h1 class="balance">£{{ total }}</h1>
+</template>
+
+<script setup>
+const props = defineProps({
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
